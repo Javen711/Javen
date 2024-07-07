84f1fe7863b1e3af18dce5fab73903d30b616201
peter-tabani <peter.tabani@strathmore.edu>
2024-07-08 02:35:47 +0300

    converting all the files to php

---
+++ pricing.php
@@ -0,0 +1,45 @@
+<section class="features2">
+    <h1>Boost Your Earnings</h1>
+    <p>Book high-value meetings in seconds, turning scheduling into a strategic advantage.</p>
+    <div class="row">
+        <div class="course-col2">
+            <img src="images/setup.png">
+            <div class="blog-right">
+                <h3>Speed Up Sales</h3>
+                <p>Keep momentum in your sales by removing scheduling barriers, allowing you to focus on closing deals.</p>
+            </div> 
+        </div>
+        <div class="blog-right">
+            <h3>Confident Closures</h3>
+            <p>Maximize success with personalized reminders and follow-up strategies integrated with your sales tools.</p>
+        </div>
+        <div class="block-right">
+            <h3>Explore Opportunities</h3>
+            <p>Discover features and strategies to drive your business growth to new heights.</p>
+        </div>
+    </div>
+  </section>
+
+  <!----EVENTS------>
+  <section class="campus">
+    <div class="row">
+        <div class="campus-col">
+            <img src="images/conference1.webp">
+            <div class="layer">
+                <h3>EVENT REMINDERS</h3>
+            </div>
+        </div>
+        <div class="campus-col">
+            <img src="images/conference3.webp">
+            <div class="layer">
+                <h3>NOTIFICATIONS</h3>
+            </div>
+        </div>   
+        <div class="campus-col">
+            <img src="images/conferencep.webp">
+            <div class="layer">
+                <h3>SCHEDULE MANAGEMENT</h3>
+            </div>
+        </div>
+    </div>
+  </section>
---
+++ signing-in.php
@@ -0,0 +1,20 @@
+<section id="login-container">
+    <section id="signup">
+      <h2>Create an Account</h2>
+      <form action="/signup" method="post">
+        <label for="signup-username">Username:</label>
+        <input type="text" id="signup-username" name="username" required>
+        
+        <label for="signup-email">Email:</label>
+        <input type="email" id="signup-email" name="email" required>
+        
+        <label for="signup-password">Password:</label>
+        <input type="password" id="signup-password" name="password" required>
+        
+        <input type="submit" value="Sign Up">
+      </form>
+    </section>
+    
+    <a href="javascript:void(0);" onclick="toggleSection()" id="toggleLink" class="center-link">Already have an account? Log in</a>
+    
+  </section>
